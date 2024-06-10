6e87ea37d206cbef40fc55b879deaebd761c9ed2
Mohammad Massoudi <m.massoudi32@gmail.com>
2024-06-10 20:45:01 +0330

    Feature: basic temprature read

--- tmp117.c
+++ tmp117.c
@@ -5,13 +5,30 @@
 
 
 static struct tmp117_config config;
+static struct tmp117_cmd cmd;
 
 
 int
 tmp117_init(struct tmp117_config *conf) {
-    memcpy(&config, conf, sizeof(struct tmp117_config));
-    tmp117_set_gpio_polarity(struct tmp117_config *state) {
-    printf("configuration done\n");
+    config.address = conf->address;
+    cmd.device_address = conf->address;
+    config.communicate = conf->communicate;
+    // tmp117_set_gpio_polarity(conf);
+    /* only need to configure 0x01 register once */
+    return TMP117_OK;
+}
+
+
+int
+tmp117_check(struct tmp117_config *conf) {
+    cmd.type = TMP117_READ;
+    cmd.reg_address = TMP117_ID_REG;
+    conf->communicate(&cmd);
+    unsigned int tmp117_id = TMP117_RD_CONCAT(cmd.rd_data[0], cmd.rd_data[1]);
+
+    if ((tmp117_id & TMP117_ID_MASK) != TMP117_ID) {
+        return ENO_TMP117_INVALID;
+    }
     return TMP117_OK;
 }
 
@@ -27,31 +44,57 @@ int
 tmp117_enable_continousconversion() {
     /* reg[0x01][11] = 0*/
     /* reg[0x01][10] = 0*/
-
     return TMP117_OK;
+}
+
+
+static int
+tmp117_data_ready(struct tmp117_config *conf) {
+    cmd.type = TMP117_READ;
+    cmd.reg_address = TMP117_CONFIG_REG;
+    conf->communicate(&cmd);
+    unsigned int reg_value = (cmd.rd_data[0] << 8) | cmd.rd_data[1];
+
+    if (reg_value & TMP117_DATA_READY_BIT) {
+        return TMP117_OK;
+    }
+    return TMP117_DATA_UNAVAILABLE;
 }
 
 
 int
-tmp117_read_temprature(double *temp) {
-    /* first read the data ready value */
-    /* temp = reg[0x00] * 0.0078125 */
+tmp117_read_temprature(struct tmp117_config *conf, float *temp) {
+    int rc = tmp117_data_ready(conf);
+    if (rc == TMP117_OK) {
+        cmd.type = TMP117_READ;
+        cmd.reg_address = TMP117_TEMPRATURE_REG;
+        conf->communicate(&cmd);
+        unsigned int reg_value = TMP117_RD_CONCAT(cmd.rd_data[0],
+                cmd.rd_data[1]);
+        *temp = reg_value * TMP117_RESOLUTION;
+        return TMP117_OK;
+    }
     return TMP117_DATA_UNAVAILABLE;
-    return TMP117_OK;
 }
 
 
-/* this function will make alert pin act as data ready */
 int
 tmp117_enable_alert(struct tmp117_config *state) {
-    /* here we need to enable interrupt alert pin */
-    /* consider the polarity and enable interrupt */
     return TMP117_OK;
 }
 
 
 static int
 tmp117_set_gpio_polarity(struct tmp117_config *state) {
-    /* assemble the packet and call the sending function */
+    char conf;
+    if (state->alert_gpio_polarity == TMP117_ALERT_ACTIVE_LOW) {
+        // TMP117_CLEAR_BIT(conf, TMP117_ALERT_POLARITY);
+    }
+    else if(state->alert_gpio_polarity == TMP117_ALERT_ACTIVE_HIGH) {
+        // TMP117_SET_BIT(conf, TMP117_ALERT_POLARITY);
+    }
+    else {
+        return ENO_TMP117_INVALID;
+    }
     return TMP117_OK;
 }
--- tmp117.h
+++ tmp117.h
@@ -1,16 +1,53 @@
 #ifndef TMP117_H_
 #define TMP117_H_
 
-#define ENO_TMP117_NORESPONSE      -1
+#define ENO_TMP117_NORESPONSE   -1
+#define ENO_TMP117_INVALID      -2
 
-#define EVENT_TMP117_LOWALERT  0
-#define EVENT_TMP117_HIGHALERT 1
+#define TMP117_OK                0
+#define TMP117_DATA_UNAVAILABLE  1
 
-#define TMP117_OK 0
-#define TMP117_DATA_UNAVAILABLE 1
+#define TMP117_READ              0
+#define TMP117_WRITE             1
 
-typedef int (*tmp117_communicate) (unsigned char *data,
-        unsigned char *response);
+#define TMP117_REG_LEN           2
+
+#define EVENT_TMP117_LOWALERT    0
+#define EVENT_TMP117_HIGHALERT   1
+
+#define TMP117_ENABLE_ALERT      0x10
+#define TMP117_ALERT_POLARITY    0x08
+#define TMP117_ALERT_ACTIVE_LOW  0x00
+#define TMP117_ALERT_ACTIVE_HIGH 0x01
+
+#define TMP117_ID_REG            0x0F
+#define TMP117_ID                0x0117
+#define TMP117_ID_MASK           0x0FFF
+
+#define TMP117_TEMPRATURE_REG    0x00
+#define TMP117_CONFIG_REG        0x01
+
+#define TMP117_RESOLUTION      0.0078125
+
+#define TMP117_DATA_READY_POS  13
+#define TMP117_DATA_READY_BIT  (0x01 << TMP117_DATA_READY_POS)
+
+#define TMP117_SET_REG(r, f)   r |= f
+#define TMP117_CLEAR_REG(r, f) r &= ~f
+
+#define TMP117_RD_CONCAT(msb, lsb)  (msb << 8) | lsb
+
+
+struct tmp117_cmd {
+    int type;
+    int device_address;
+    int reg_address;
+    unsigned char wr_data[TMP117_REG_LEN];
+    unsigned char rd_data[TMP117_REG_LEN];
+};
+
+
+typedef int (*tmp117_communicate) (struct tmp117_cmd *cmd);
 
 
 struct tmp117_config {
@@ -29,13 +66,17 @@ int
 tmp117_init(struct tmp117_config *tmp117);
 
 
+int
+tmp117_check(struct tmp117_config *conf);
+
+
 /* we need to pass opaque pointers to function */
 int
 tmp117_get_alert(struct tmp117_config *state);
 
 
 int
-tmp117_read_temprature(double *temp);
+tmp117_read_temprature(struct tmp117_config *conf, float *temp);
 
 
 /* this function will make alert pin act as data ready */
